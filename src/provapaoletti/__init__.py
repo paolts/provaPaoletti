8ff30d384787fcdc7da0f76fa9177ecad963686a
Sebot79 <sebastianoamadio09@gmail.com>
2026-02-01 15:35:38 +0100

    Update __init__.py

--- src/provapaoletti/__init__.py
+++ src/provapaoletti/__init__.py
@@ -1,5 +1,10 @@
 # Leonardo Paoletti
+#modificata da seba
 # prima prova sanguinosa con git, github, uv e python
 
 def main() -> None:
     print("Se funziona, Daje Roma Daje")
+    import datetime
+    oggi = datetime.datetime.now()
+    print("oggi è", oggi)
+main()
